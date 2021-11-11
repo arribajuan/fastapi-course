dfc2e79c5149b5772293ed62147efbb095f233d6
Juan Martinez <jmartinez@benchsci.com>
2021-11-11 10:30:28 -0500

    Added methods for postrs and postman tests

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -6,3 +7,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your list of posts"}
+
+
+@app.post("/posts")
+def post_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: '{payload['title']}'',  content: '{payload['content']}'"}
